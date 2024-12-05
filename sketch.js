f40e4620c9dd156943c431d1d6d0dcc2f8a3fd4c
George1602181 <1602181@spsd.sk.ca>
2024-12-05 10:24:24 -0600

    got basic versions of the vehichles to show up just need them to pick a direction a keep going instead of changing directions

--- sketch.js
+++ sketch.js
@@ -20,40 +20,79 @@ class Vehichle {
     fill(this.color);
   }
 
-  move() {
-    speed = random(0, 10);
-    this.x += speed;
+  moveOneWay() {
+    this.speed = random(0, 10);
+    if (random(100) > 50) {
+      this.x += this.speed;
+    }
   }
+
+  // moveOtherWay() {
+  //   if (random(100) > 50) {
+  //     this.x -= this.speed;
+  //   }
+  // }
 }
 
-class Car extends Vehichle {
+class Car1 extends Vehichle {
   constructor(x, y, speed, color, width, length) {
     super(x, y, speed, color, width, length);
   }
 
   display() {
     super.display();
-    rect(this.x, this.y, this.width, this.length);
+    rect(this.x, this.y, this.speed, this.color, this.width, this.length);
   }
 }
 
-class Bus extends Vehichle {
+class Car2 extends Vehichle {
+  constructor(x, y, speed, color, width, length) {
+    super(x, y, speed, color, width, length);
+  }
+
+  display() {
+    super.display();
+    rect(this.x, this.y, this.speed, this.color, this.width, this.length);
+  }
+}
+
+class Bus1 extends Vehichle {
   constructor(x, y, speed, color, width, length) {
     super(x, y, speed, color, width, length);
   }
   display() {
     super.display();
-    rect(this.x, this.y, this.width, this.length);
+    rect(this.x, this.y, this.speed, this.color, this.width, this.length);
   }
 }
 
-class Bike extends Vehichle {
+class Bus2 extends Vehichle {
   constructor(x, y, speed, color, width, length) {
     super(x, y, speed, color, width, length);
   }
   display() {
     super.display();
-    rect (this.x, this.y, this.width, this.length);
+    rect(this.x, this.y, this.speed, this.color, this.width, this.length);
+  }
+}
+
+class Bike1 extends Vehichle {
+  constructor(x, y, speed, color, width, length) {
+    super(x, y, speed, color, width, length);
+  }
+  display() {
+    super.display();
+    rect (this.x, this.y, this.speed, this.color, this.width, this.length);
+  }
+}
+
+class Bike2 extends Vehichle {
+  constructor(x, y, speed, color, width, length) {
+    super(x, y, speed, color, width, length);
+  }
+  display() {
+    super.display();
+    rect (this.x, this.y, this.speed, this.color, this.width, this.length);
   }
 }
 
@@ -61,8 +100,27 @@ let theVehichle = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < 10; i++) {
+    if (random(100) > 33) {
+      let someCar = new Car1(random(width), random(height), random(0, 10), random(255), random(255), random(255), 10, 30);
+      theVehichle.push(someCar);
+    }
+    else if (random(100) > 33) {
+      let someBus = new Bus1(random(width), random(height), random(0, 10), random(255), random(255), random(255), 10, 100);
+      theVehichle.push(someBus);
+    }
+    else {
+      let someBike = new Bike1(random(width), random(height), random(0, 10), random(255), random(255), random(255), 5, 7);
+      theVehichle.push(someBike);
+    }
+  }
 }
 
 function draw() {
   background(220);
+  for(let aVehichle of theVehichle) {
+    aVehichle.moveOneWay();
+    aVehichle.moveOtherWay();
+    aVehichle.display();
+  }
 }
