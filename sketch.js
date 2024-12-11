4b8382a2f1e0e33d61b72a1ffb15d7c32ec66ed8
George1602181 <1602181@spsd.sk.ca>
2024-12-11 11:02:00 -0600

    implemented a new strategy for the vehichle's movement

--- sketch.js
+++ sketch.js
@@ -6,107 +6,106 @@
 // - describe what you did to take this project "above and beyond"
 
 class Vehichle {
-  constructor(x, y, width, length, speed, color, ) {
+  constructor(x, y, width, length, speed, color, direction) {
     this.x = x;
     this.y = y;
     this.speed = speed;
     this.color = color;
     this.width = width;
     this.length = length;
+    this.direction = direction;
   }
 
   display () {
     fill(this.color);
   }
-
-  moveOneWay() {
-    this.speed = random(0, 10);
-    this.x += this.speed;
-  }
-
-  moveOtherWay() {
-    this.speed = random(0, 10);
-    this.x -= this.speed;
+  move() {
+    if(this.direction === 1) {
+      this.speed += this.speed;
+    }
+    else if (this.direction === 2) {
+      this.speed -= this.speed;
+    }
   }
 }
 
 class Car1 extends Vehichle {
-  constructor(x, y, width, length, speed, color) {
-    super(x, y, width, length, speed, color);
+  constructor(x, y, width, length, speed, color, direction) {
+    super(x, y, width, length, speed, color, direction);
   }
 
   display() {
     super.display();
-    rect(this.x, this.y, this.width, this.length, this.speed, this.color);
+    rect(this.x, this.y, this.width, this.length, this.speed, this.color, this.direction);
   }
   move() {
-    super.moveOneWay;
+    super.move();
   }
 }
 
 class Car2 extends Vehichle {
-  constructor(x, y, width, length, speed, color) {
-    super(x, y, width, length, speed, color);
+  constructor(x, y, width, length, speed, color, direction) {
+    super(x, y, width, length, speed, color, direction);
   }
 
   display() {
     super.display();
-    rect(this.x, this.y, this.width, this.length, this.speed, this.color);
+    rect(this.x, this.y, this.width, this.length, this.speed, this.color, this.direction);
   }
   move() {
-    super.moveOtherWay;
+    super.move();
   }
 }
 
 class Bus1 extends Vehichle {
-  constructor(x, y, width, length, speed, color) {
-    super(x, y, width, length, speed, color);
+  constructor(x, y, width, length, speed, color, direction) {
+    super(x, y, width, length, speed, color, direction);
   }
   display() {
     super.display();
-    rect(this.x, this.y, this.width, this.length, this.speed, this.color);
+    rect(this.x, this.y, this.width, this.length, this.speed, this.color, this.direction);
   }
   move() {
-    super.moveOneWay;
+    super.move();
   }
 }
 
 class Bus2 extends Vehichle {
-  constructor(x, y, width, length, speed, color) {
-    super(x, y, width, length, speed, color);
+  constructor(x, y, width, length, speed, color, direction) {
+    super(x, y, width, length, speed, color, direction);
   }
   display() {
     super.display();
-    rect(this.x, this.y, this.width, this.length, this.speed, this.color);
+    rect(this.x, this.y, this.width, this.length, this.speed, this.color, this.direction);
   }
   move() {
-    super.moveOtherWay;
+    super.move();
   }
 }
 
 class Bike1 extends Vehichle {
-  constructor(x, y, width, length, speed, color) {
-    super(x, y, width, length, speed, color);
+  constructor(x, y, width, length, speed, color, direction) {
+    super(x, y, width, length, speed, color, direction);
   }
   display() {
     super.display();
-    rect(this.x, this.y, this.width, this.length, this.speed, this.color);
+    rect(this.x, this.y, this.width, this.length, this.speed, this.color, this.direction);
   }
   move() {
-    super.moveOneWay;
+    super.move();
   }
 }
 
 class Bike2 extends Vehichle {
-  constructor(x, y, width, length, speed, color) {
-    super(x, y, width, length, speed, color);
+  constructor(x, y, width, length, speed, color, direction) {
+    super(x, y, width, length, speed, color, direction);
   }
   display() {
     super.display();
-    rect(this.x, this.y, this.width, this.length, this.speed, this.color);
+    rect(this.x, this.y, this.width, this.length, this.speed, this.color, this.direction);
   }
   move() {
-    this.moveOtherWay;
+    super.move();
   }
 }
 
@@ -137,27 +136,27 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   for (let i = 0; i < 10; i++) {
     if (random(100) > 16) {
-      let someCar = new Car1(random(width), random(height), 100, 30, random(0, 10), random(255));
+      let someCar = new Car1(random(width), random(height), 100, 30, random(0, 10), random(255), 1);
       theVehichle.push(someCar);
     }
     else if (random(100) > 16) {
-      let someCar2 = new Car2(random(width), random(height), 100, 30, random(0, 10), random(255));
+      let someCar2 = new Car2(random(width), random(height), 100, 30, random(0, 10), random(255), 2);
       theVehichle.push(someCar2);
     }
     else if (random(100) > 16) {
-      let someBus = new Bus1(random(width), random(height), 200, 30, random(0, 10), random(255));
+      let someBus = new Bus1(random(width), random(height), 200, 30, random(0, 10), random(255), 1);
       theVehichle.push(someBus);
     }
     else if (random(100) > 16) {
-      let someBus2 = new Bus2(random(width), random(height), 200, 30, random(0, 10), random(255));
+      let someBus2 = new Bus2(random(width), random(height), 200, 30, random(0, 10), random(255), 2);
       theVehichle.push(someBus2);
     }
     else if (random(100) > 16) {
-      let someBike = new Bike1(random(width), random(height), 10, 5, random(0, 10), random(255));
+      let someBike = new Bike1(random(width), random(height), 10, 5, random(0, 10), random(255), 1);
       theVehichle.push(someBike);
     }
     else {
-      let someBike2 = new Bike2(random(width), random(height), 10, 5, random(0, 10), random(255));
+      let someBike2 = new Bike2(random(width), random(height), 10, 5, random(0, 10), random(255), 2);
       theVehichle.push(someBike2);
     }
   }
@@ -168,8 +167,7 @@ function setup() {
 function draw() {
   background(220);
   for(let aVehichle of theVehichle) {
-    aVehichle.moveOneWay();
-    aVehichle.moveOtherWay();
+    aVehichle.move();
     aVehichle.display();
   }
   for (let aChicken of theChicken) {
